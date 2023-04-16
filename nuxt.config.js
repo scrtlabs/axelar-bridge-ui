08d2f578855479bd4dc787358b85c52f0d627d32
Cashmaney <itzik@enigma.co>
2023-04-16 17:31:41 +0300

    Okay, time to see if this is worth anything

--- nuxt.config.js
+++ nuxt.config.js
@@ -86,9 +86,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
-    "@nuxt/image"
-    // '@nuxtjs/style-resources'
+    '@nuxtjs/vuetify'
   ],
 
   image: {
