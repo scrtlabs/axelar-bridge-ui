3a32b67f4cd77e783a9574cc016f2e3fcd30650b
Tovi Levis <tovi@fhenix.io>
2024-11-20 15:05:59 +0200

    update secret.js to 1.15.0-beta.2

--- nuxt.config.js
+++ nuxt.config.js
@@ -99,6 +99,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: ['secretjs'],
     extend(config, ctx) {
       if (ctx.isDev) {
         config.devtool = ctx.isClient ? 'source-map' : 'inline-source-map'
